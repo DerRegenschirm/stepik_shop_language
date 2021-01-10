7ee7d52589d2d402e636ab2d56bb6f20cc585c77
DerRegenschirm <obuhovavohubo@gmail.com>
2021-01-10 21:23:11 +0200

    language choose

--- conftest.py
+++ conftest.py
@@ -5,9 +5,12 @@ from selenium import webdriver
 @pytest.fixture(scope="function")
 def browser(request):
     print("\nstart browser for test..")
+
+    language = request.config.getoption("language")
+
     # languages for Chrome
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
